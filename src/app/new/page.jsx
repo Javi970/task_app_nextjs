620b9ddd0084a1e81259c9b28648fb7434b05cbf
Javi970 <javierpaivag@gmail.com>
2023-06-06 16:08:05 -0300

    add make tasks

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useTasks } from "@/context/TaskContext";
+import { useRouter } from "next/navigation";
 
 function Page() {
-  return <div>task form</div>;
+  const [task, setTask] = useState();
+  const { createTask } = useTasks();
+  const router = useRouter();
+  const handleChange = (e) =>
+    setTask({ ...task, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask(task.title, task.description);
+    router.push("/");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="title" placeholder="Write a title" onChange={handleChange} />
+      <textarea
+        name="description"
+        placeholder="Write a description"
+        onChange={handleChange}
+      />
+      <button>Save</button>
+    </form>
+  );
 }
 
 export default Page;
