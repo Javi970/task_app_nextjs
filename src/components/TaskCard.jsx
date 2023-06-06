732c00b449e4aacd3a2849019a03aab9647ed9b5
Javi970 <javierpaivag@gmail.com>
2023-06-06 16:26:05 -0300

    add delete task

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,14 +1,24 @@
 import { useRouter } from "next/navigation";
+import { useTasks } from "@/context/TaskContext";
 
 export const TaskCard = ({ task }) => {
   const router = useRouter();
+  const { deleteTask } = useTasks();
   return (
     <div
       style={{ background: "#202020", color: "white" }}
       onClick={() => router.push(`/edit/${task.id}`)}
     >
       <h1>{task.title}</h1>
-      <button>Delete</button>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          const accept = window.confirm("are you sure?");
+          if (accept) deleteTask(task.id);
+        }}
+      >
+        Delete
+      </button>
       <p>{task.description}</p>
     </div>
   );
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -28,7 +28,7 @@ export const TaskProvider = ({ children }) => {
       description: "some third task",
     },
   ]);
-
+  //esta funcion agrega las tareas
   const createTask = (title, description) =>
     setTasks([
       ...tasks,
@@ -38,9 +38,11 @@ export const TaskProvider = ({ children }) => {
         id: uuid(),
       },
     ]);
+  const deleteTask = (id) =>
+    setTasks([...tasks.filter((task) => task.id !== id)]);
 
   return (
-    <TaskContext.Provider value={{ tasks, createTask }}>
+    <TaskContext.Provider value={{ tasks, createTask, deleteTask }}>
       {children}
     </TaskContext.Provider>
   );
